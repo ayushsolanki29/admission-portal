548de4af72849c16937d5cb773ec3801685b4359
Shaikh-Adnan28 <shaikhadnan2834@gmail.com>
2025-02-24 22:47:33 +0530

    College -deatile Page - hero sec , course-section-.

---
+++ college-details.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+    <?php include 'php/pages/meta.php' ?>
+    <link rel="stylesheet" href="assets\css\colleged.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+</head>
+
+<body>
+
+
+    <?php include 'php/pages/header.php' ?>
+
+    <!-- slide-bar start -->
+    <aside class="slide-bar">
+        <div class="close-mobile-menu">
+            <a href="javascript:void(0);"><i class="fas fa-times"></i></a>
+        </div>
+
+        <!-- offset-sidebar start -->
+        <div class="offset-sidebar">
+            <div class="offset-widget offset-logo mb-30">
+                <a href="index.html">
+                    <img src="assets/img/logo/header_logo_one.svg" alt="logo">
+                </a>
+            </div>
+            <div class="offset-widget mb-40">
+                <div class="info-widget">
+                    <h4 class="offset-title mb-20">About Us</h4>
+                    <p class="mb-30">
+                        But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain
+                        was born and will give you a complete account of the system and expound the actual teachings of
+                        the great explore
+                    </p>
+                    <a class="theme_btn theme_btn_bg" href="contact.html">Contact Us</a>
+                </div>
+            </div>
+            <div class="offset-widget mb-30 pr-10">
+                <div class="info-widget info-widget2">
+                    <h4 class="offset-title mb-20">Contact Info</h4>
+                    <p> <i class="fal fa-address-book"></i> 23/A, Miranda City Likaoli Prikano, Dope</p>
+                    <p> <i class="fal fa-phone"></i> +0989 7876 9865 9 </p>
+                    <p> <i class="fal fa-envelope-open"></i> info@example.com </p>
+                </div>
+            </div>
+        </div>
+        <!-- offset-sidebar end -->
+
+        <!-- side-mobile-menu start -->
+        <nav class="side-mobile-menu">
+            <ul id="mobile-menu-active">
+                <li class="has-dropdown">
+                    <a href="index.html">Home</a>
+                    <ul class="sub-menu">
+                        <li><a href="index.html">Home Style 1</a></li>
+                        <li><a href="index-2.html">Home Style 2</a></li>
+                        <li><a href="index-3.html">Home Style 3</a></li>
+                    </ul>
+                </li>
+                <li><a href="about.html">About</a></li>
+                <li class="has-dropdown">
+                    <a href="#">Pages</a>
+                    <ul class="sub-menu">
+                        <li><a href="courses.html">Course One</a></li>
+                        <li><a href="courses-2.html">Course Two</a></li>
+                        <li><a href="course-details.html">Courses Details</a></li>
+                        <li><a href="price.html">Price</a></li>
+                        <li><a href="instructor.html">Instructor</a></li>
+                        <li><a href="instructor-profile.html">Instructor Profile</a></li>
+                        <li><a href="faq.html">FAQ</a></li>
+                        <li><a href="login.html">login</a></li>
+                    </ul>
+                </li>
+                <li class="has-dropdown"><a href="#">Blogs</a>
+                    <ul class="sub-menu">
+                        <li><a href="blog.html">Blog Grid</a></li>
+                        <li><a href="blog-details.html">Blog Details</a></li>
+                    </ul>
+                </li>
+                <li><a href="contact.html">Contacts Us</a></li>
+            </ul>
+        </nav>
+        <!-- side-mobile-menu end -->
+    </aside>
+    <div class="body-overlay"></div>
+
+    <!-- slide-bar end -->
+
+    <main>
+        <!-- Fisrt-section  -->
+        <div class="coantiner-fluids d-flex">
+            <div class="left-section col-md-6 p-5">
+                <h3 class="p-2">IIT Madras - Admission 2025, Fees,<br> Courses, Placement, Ranking</h3>
+
+                <p class="p-2" style="color: black; font-weight: 500;">
+                    IIT Madras B.Tech, BS + MS and B.Tech+M.Tech admission is based on JEE Advanced and JEE Main. The college offers 92 courses at the PG and UG levels. For admission to the MBA registration is open, furthermore, CAT 2025 will be held on November 30, 2025. IIT Madras fees range from Rs 5,000 to Rs 3,00,000 annually.
+                </p>
+                <div class="p-2 ">
+                    <i class="fa-solid fa-location-dot d-flex align-items-center gap-2">
+                        <p style="color:#EB571C">Chennai (Tamil Nadu)</p>
+                    </i>
+                </div>
+                <div class="p-2 mt-5 gap-5 d-flex">
+                    <button class="btn p-2" style="border:2px solid #EB571C; color:#EB571C; border-radius:40px;">Download Bronchure</button>
+                    <button class="btn p-2 px-5" style="border:2px solid #EB571C; background-color: #EB571C; color: white; border-radius:40px;">Shorlist</button>
+                </div>
+            </div>
+            <div class="right-section ban col-md-6 d-flex align-items-center">
+                <div class="ban-back">
+                    <img src="assets\img\college\banner1.avif" alt="" srcset="">
+                </div>
+            </div>
+
+        </div>
+        <!-- End First Section  -->
+
+        <!-- second section  -->
+        <div class="container-fluid p-5">
+            <h5>About IIT Madras</h5>
+            <p class="mt-4"> Institute of Technology Madras was established in 1959. As per the QS World University Ranking, IIT Madras bagged the 526th. IIT Madras campus is spread across 630 acres of land and has over 550 faculty, 9700 students and 1250 administrative & supporting staff. IITM courses are spread across 16 academic departments. It offers multiple courses at the undergraduate, postgraduate and doctoral levels. Apart from the regular courses, it also offers an online B.Sc programme. IIT Chennai admissions in all these programmes are done based on entrance exams. UG admissions are done through JEE Advanced, and admission to the MA and M.Tech programmes are done through GATE. Candidates willing to take admission in the MBA and M.Sc programme need to appear for CAT and JAM respectively.</p>
+        </div>
+        <!-- second end section -->
+
+        <!-- banner-download-part  -->
+        <div class="banner-part container-fluid">
+            <h3>Want To Know About IIT Madras</h3>
+            <button class="b-b">Download Brochure</button>
+        </div>
+        </div>
+        <!-- banner-download-part End -->
+
+        <!-- Course And addmision 2025  -->
+        <div class="container-fluid mt-5">
+            <div class="top-heading-text p-4">
+                <h4 class="mt-2">IIT Madras Courses And Admission 2025</h4>
+                <p class="mt-4">IIT Madras Courses and Admissions 2025
+                    IIT Madras has 16 academic departments and a few advanced research centres in various disciplines of pure sciences and engineering. The curriculum of IIT Madras is designed in a way to encourage students to balance both academics and research. Several undergraduate, postgraduate, integrated, and doctoral-level courses are available at IIT Madras. It provides B.Tech and dual degree programmes in twelve different disciplines. The dual degree (BS & MS) in biological sciences and physics is available. MA, M.Sc, M.Tech, and MBA programmes are available at IIT Madras at the postgraduate level. Other IIT Madras courses available at the postgraduate level include the AICTE-sponsored QIP, the EMBA programme, and the web-enabled M.Tech programme. There are also numerous research programmes available, such as the Joint PhD and AICTE QIP. For all the courses, the IITM fees is different.</p>
+            </div>
+            <div class="container-fluid card-sec">
+                <div class="card-cour">
+                    <div class="head ">
+                        <h3>MSc</h3>
+                    </div>
+                    <div class="body-card">
+                        <p class="d-flex justify-content-around"><strong>₹6,000</strong> <span>2 - 5 Years</span> <span>Regular</span></p>
+                        <p>Exams Accepted: IIT JAM</p>
+                        <p>Courses Offered: M.Sc in Chemistry, Physics</p>
+                        <p><strong>Eligibility:</strong> Bachelor's degree in Science with minimum 55% marks.</p>
+                        <div class="button-card d-flex align-items-center justify-content-center gap-3">
+                            <button class="btn one-b p-2">Talk to Experts</button>
+                            <button class="btn  p-2">Download Brochure</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-cour">
+                    <div class="head ">
+                        <h3>MSc</h3>
+                    </div>
+                    <div class="body-card">
+                    <p class="d-flex justify-content-around"><strong>₹6,000</strong> <span>2 - 5 Years</span> <span>Regular</span></p>
+                        <p >Exams Accepted: IIT JAM</p>
+                        <p>Courses Offered: M.Sc in Chemistry, Physics</p>
+                        <p><strong>Eligibility:</strong> Bachelor's degree in Science with minimum 55% marks.</p>
+                        <div class="button-card align-items-center justify-content-center d-flex gap-3">
+                            <button class="btn one-b p-2">Talk to Experts</button>
+                            <button class="btn  p-2">Download Brochure</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-cour">
+                    <div class="head ">
+                        <h3>MSc</h3>
+                    </div>
+                    <div class="body-card">
+                    <p class="d-flex justify-content-around"><strong>₹6,000</strong> <span>2 - 5 Years</span> <span>Regular</span></p>
+                        <p>Exams Accepted: IIT JAM</p>
+                        <p>Courses Offered: M.Sc in Chemistry, Physics</p>
+                        <p><strong>Eligibility:</strong> Bachelor's degree in Science with minimum 55% marks.</p>
+                        <div class="button-card align-items-center justify-content-center d-flex gap-3">
+                            <button class="btn one-b p-2">Talk to Experts</button>
+                            <button class="btn p-2">Download Brochure</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </main>
+    <?php include 'php/pages/footer.php' ?>
+
+</body>
+
+</html>
